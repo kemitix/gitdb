c8cf933a337124a15546fcb64a963242e9ff86cc
Paul Campbell <pcampbell@kemitix.net>
2018-05-11 19:51:58 +0100

    Initialise DB

--- src/main/scala/net/kemitix/gitdb/GitDB.scala
+++ src/main/scala/net/kemitix/gitdb/GitDB.scala
@@ -1,5 +1,27 @@
 package net.kemitix.gitdb
 
-class GitDB {
+import java.nio.file.Path
 
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
+import scala.beans.BeanProperty
+
+class GitDB (val dir: Path) {
+
+  @BeanProperty
+  val repository: Repository = {
+    val fileRepositoryBuilder = new FileRepositoryBuilder
+    fileRepositoryBuilder.setGitDir(dir.toFile)
+    fileRepositoryBuilder.build()
+  }
+
+  def close: Unit = repository.close()
+
+}
+
+object GitDB {
+  def init(dir: Path): GitDB = {
+    new GitDB(dir)
+  }
 }
--- src/test/java/net/kemitix/gitdb/GitDBTest.java
+++ src/test/java/net/kemitix/gitdb/GitDBTest.java
@@ -1,13 +1,41 @@
 package net.kemitix.gitdb;
 
+import org.eclipse.jgit.lib.Repository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GitDBTest {
 
+    private final Path dbDir = Files.createTempDirectory("gitdb");
+    private final GitDB gitDB = GitDB.init(dbDir);
+
+    GitDBTest() throws IOException {
+    }
+
     @Test
-    void dummy() {
-        assertThat(true).isTrue();
+    void shouldInitialiseGitDB() {
+        //then
+        assertThat(gitDB).isNotNull();
+        assertThat(gitDB.dir()).isDirectory()
+                .isEqualTo(dbDir);
+        final Repository repository = gitDB.getRepository();
+        assertThat(repository.isBare()).isTrue();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        gitDB.close();
+        Files.walk(dbDir)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
     }
 }
