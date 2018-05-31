5ffdb0f8952d734ac411c557ef0c081882a09fb2
Paul Campbell <pcampbell@kemitix.net>
2018-05-31 23:30:35 +0100

    Add LICENSE.txt

--- src/main/java/net/kemitix/gitdb/package-info.java
+++ src/main/java/net/kemitix/gitdb/package-info.java
@@ -20,11 +20,3 @@
  */
 
 package net.kemitix.gitdb;
-
-//import net.kemitix.mon.TypeAlias;
-
-//public class Reference extends TypeAlias<String> {
-//    protected Reference(String value) {
-//        super(value);
-//    }
-//}
